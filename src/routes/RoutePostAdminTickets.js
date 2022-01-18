dd1bc85b7ad7cb587f83c4ace4581402a2768ee7
[sanbonai06] <sanbonai06@naver.com>
2022-01-18 23:26:44 +0900

    공짜 티켓 코드 수정

--- src/routes/RoutePostAdminTickets.js
+++ src/routes/RoutePostAdminTickets.js
@@ -2,19 +2,14 @@ const express = require('express');
 // const axios = require("axios");
 const RoutePostAdminTickets = express.Router();
 const { validationCatch } = require('../middleware/validationCatch');
-const { query, body } = require('express-validator');
+const { body } = require('express-validator');
 const { ServerCommonError, CustomError } = require('../errors');
-const { Ticket, Admin } = require('../model');
+const { Ticket } = require('../model');
 const { AdminAuthentication } = require('../middleware');
 RoutePostAdminTickets.post(
   '/admin/tickets',
   AdminAuthentication,
   [
-    body('phoneNumber')
-      .matches(/^[0-9]+$/)
-      .withMessage('숫자만 들어와야합니다.')
-      .isLength({ min: 11, max: 11 })
-      .withMessage('전화번호 길이는 11자이어야 합니다.'),
     body('ticketCount')
       .isInt()
       .withMessage('ticketCount 0이상 10이하의 정수로 필요합니다.')
@@ -23,8 +18,7 @@ RoutePostAdminTickets.post(
   async (req, res, next) => {
     try {
       // 추후 phoneNumber 는 accessToken 미들웨어에서 가져올 예정입니다./
-      const { phoneNumber, ticketCount, adminUser } = req.body;
-      console.log(phoneNumber, ticketCount, adminUser);
+      const { ticketCount, adminUser } = req.body;
 
       if (ticketCount <= 0 || ticketCount > 10) {
         return res.custom400FailMessage('티켓수량오류');
@@ -33,18 +27,14 @@ RoutePostAdminTickets.post(
       let listOfTickets = [];
       // 티켓 전체 수량 세기
       const allTicketCount = await Ticket.find().countDocuments();
-      const getAdminObjectId = await Admin.find({
-        userId: adminUser.userId
-      });
-      console.log(getAdminObjectId[0]._id);
       for (step = 0; step < ticketCount; step++) {
         // 티켓 서버에서 발급
         const ticket = new Ticket({
           ticketNumber: allTicketCount + 1 + step,
           accountName: adminUser.name,
           adminTicket: true,
-          manager: getAdminObjectId[0]._id,
-          phoneNumber
+          manager: adminUser._id,
+          status: 'confirm-deposit'
         });
         listOfTickets.push(ticket);
       }

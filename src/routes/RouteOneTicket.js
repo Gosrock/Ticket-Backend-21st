119596899f7e1502a794732cb035abfb6ab26567
chaeshee0908 <chaeshee0908@naver.com>
2022-01-22 13:28:52 +0900

    티켓정보확인소스 문제 해결

--- src/routes/RouteOneTicket.js
+++ src/routes/RouteOneTicket.js
@@ -6,19 +6,19 @@ const { ServerCommonError, CustomError } = require('../errors');
 const { Ticket } = require('../model');
 
 RouteOneTicket.get(
-  '/tickets/:_id',
-  [param('_id').isMongoId().withMessage('몽고 아이디 형식이 아닙니다.')],
+  '/tickets/:id',
+  [param('id').isMongoId().withMessage('몽고 아이디 형식이 아닙니다.')],
   validationCatch,
   async (req, res, next) => {
     try {
-      const { ticketId } = req.params.id;
+      const ticketId = req.params.id;
 
       // 티켓 하나의 정보 받아오기
       const ticketInfo = await Ticket.findOne({
         _id: ticketId
       });
 
-      // 티켓의 정보가 없을 때 
+      // 티켓의 정보가 없을 때
       if (ticketInfo === null) {
         return res.custom400FailMessage('티켓정보없음');
       }

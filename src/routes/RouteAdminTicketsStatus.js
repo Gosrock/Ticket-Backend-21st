24b795d4932ee982ceab6ff83b22619bcb4da5f5
def30 <luvjuns30@gmail.com>
2022-01-23 23:20:59 +0900

    code 수정

--- src/routes/RouteAdminTicketsStatus.js
+++ src/routes/RouteAdminTicketsStatus.js
@@ -4,35 +4,34 @@ const { validationCatch } = require('../middleware/validationCatch');
 const { param, query, body } = require('express-validator');
 const { ServerCommonError, CustomError } = require('../errors');
 const { Ticket } = require('../model');
+const { AdminAuthentication } = require('../middleware');
 
 RouteAdminTicketsStatus.patch(
   '/admin/tickets/:_id',
+  //AdminAuthentication,
   [
     body('status')
       .isString()
       .withMessage('Status 필요합니다.')
       .isIn(['confirm-deposit', 'pending-deposit', 'enter', 'non-deposit'])
       .withMessage('Status 필요합니다.'),
-    param('id').isMongoId().withMessage('몽고아이디 형식이여야 합니다.')
+    param('_id').isMongoId().withMessage('몽고아이디 형식이여야 합니다.')
   ],
   validationCatch,
   async (req, res, next) => {
     try {
-      const { ticketStatus } = req.body;
-      const { id } = req.params._id;
+      const ticketStatus = req.body.status;
+      const id = req.params._id;
       console.log(id);
       // 요청 받은 아이디를 찾아서 그의 status를 요청받은 status로 변경해줘야 함
-      const findTicket = await Ticket.findOne({ _id: id });
-      if (!findTicket.length) {
-        return res.custom400FailMessage('티켓을 찾을 수 없음');
-      }
+
       const statusUpdate = await Ticket.findOneAndUpdate(
         { _id: id },
         { status: ticketStatus },
         { new: true }
       );
       console.log(statusUpdate);
-      return res.custom200SuccessMessage('성공');
+      return res.custom200SuccessData(statusUpdate);
       //const updateStatus = await Ticket.findByIdAndUpdate( id, status, {new:true} ).exec();
       // return res.json({ success: true, data: { user: '찬진' } });
     } catch (err) {
--- src/routes/RouteAdminTicketsinfo.js
+++ src/routes/RouteAdminTicketsinfo.js
@@ -3,60 +3,59 @@ const RouteAdminTicketsInfo = express.Router();
 const { validationCatch } = require('../middleware/validationCatch');
 const { query, body } = require('express-validator');
 const { ServerCommonError, CustomError } = require('../errors');
-const { Ticket } = require('../model');
+const { Ticket, Admin } = require('../model');
+const { AdminAuthentication } = require('../middleware');
 
 RouteAdminTicketsInfo.get(
   '/admin/tickets/info',
-  [
-    query('status')
-      .isString()
-      .withMessage('Status 필요합니다.')
-      .isIn([('confirm-deposit', 'pending-deposit', 'enter', 'non-deposit')])
-  ],
+  //AdminAuthentication,
+  [],
   validationCatch,
   async (req, res, next) => {
     try {
       //티켓관련
-      const issuedTickets = await Ticket.find({
-        status: {
-          $in: ['confirm-deposit', 'pending-deposit', 'enter', 'non-deposit']
-        }
-      }).countDocuments();
-      console.log(issuedTickets);
-      const salesProceeds =
-        (await Ticket.find({
+      let resultObject = {};
+      const [
+        issuedTickets,
+        salesProceeds,
+        confirmedDeposit,
+        pendingDeposit,
+        nonDeposit,
+        entered,
+        notEntered
+      ] = await Promise.all([
+        Ticket.countDocuments(),
+        Ticket.countDocuments({
+          status: {
+            $in: ['confirm-deposit', 'enter']
+          }
+        }),
+        Ticket.countDocuments({
           status: { $in: ['confirm-deposit', 'enter'] }
-        }).countDocuments()) * 3000;
-      console.log(salesProceeds);
-      //입금관련
-      const confirmedDeposit = await Ticket.find({
-        status: { $in: ['confirm-deposit', 'enter'] }
-      }).countDocuments();
-      console.log(confirmedDeposit);
-      const pendingDeposit = await Ticket.find({
-        status: { $in: ['pending-deposit'] }
-      }).countDocuments();
-      console.log(pendingDeposit);
-      const nonDeposit = await Ticket.find({
-        status: { $in: ['non-deposit'] }
-      }).countDocuments();
-      console.log(nonDeposit);
-      //입장확인
-      const entered = await Ticket.find({
-        status: { $in: ['enter'] }
-      }).countDocuments();
-      console.log(entered);
-      const notEntered = issuedTickets - entered;
-      console.log(notEntered);
-      return res.custom200SuccessData({
+        }),
+        Ticket.countDocuments({
+          status: { $in: ['pending-deposit'] }
+        }),
+        Ticket.countDocuments({
+          status: { $in: ['non-deposit'] }
+        }),
+        Ticket.countDocuments({
+          status: { $in: ['enter'] }
+        }),
+        Ticket.countDocuments({
+          status: { $in: ['confirm-deposit', 'pending-deposit', 'non-deposit'] }
+        })
+      ]);
+      resultObject = {
         issuedTickets: issuedTickets,
-        salesProceeds: salesProceeds,
+        salesProceeds: salesProceeds * 3000,
         confirmedDeposit: confirmedDeposit,
         pendingDeposit: pendingDeposit,
         nonDeposit: nonDeposit,
         entered: entered,
         notEntered: notEntered
-      });
+      };
+      return res.custom200SuccessData(resultObject);
       // return res.json({ success: true, data: { user: '찬진' } });
     } catch (err) {
       if (err instanceof CustomError) {

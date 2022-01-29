88bd0c922f33df9e1dbbdb7dbdf7f7961b281b59
이찬진 <water0641@naver.com>
2022-01-29 17:01:03 +0900

    add response phonenuber in auth validation

---
+++ src/routes/RouteAdminTicketsInfo.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const RouteAdminTicketsInfo = express.Router();
+const { validationCatch } = require('../middleware/validationCatch');
+const { query, body } = require('express-validator');
+const { ServerCommonError, CustomError } = require('../errors');
+const { Ticket, Admin } = require('../model');
+const { AdminAuthentication } = require('../middleware');
+
+RouteAdminTicketsInfo.get(
+  '/admin/tickets/info',
+  AdminAuthentication,
+  [],
+  validationCatch,
+  async (req, res, next) => {
+    try {
+      //티켓관련
+      let resultObject = {};
+      const [
+        issuedTickets,
+        salesProceeds,
+        confirmedDeposit,
+        pendingDeposit,
+        nonDeposit,
+        entered,
+        notEntered
+      ] = await Promise.all([
+        Ticket.countDocuments(),
+        Ticket.countDocuments({
+          status: {
+            $in: ['confirm-deposit', 'enter']
+          }
+        }),
+        Ticket.countDocuments({
+          status: { $in: ['confirm-deposit', 'enter'] }
+        }),
+        Ticket.countDocuments({
+          status: { $in: ['pending-deposit'] }
+        }),
+        Ticket.countDocuments({
+          status: { $in: ['non-deposit'] }
+        }),
+        Ticket.countDocuments({
+          status: { $in: ['enter'] }
+        }),
+        Ticket.countDocuments({
+          status: { $in: ['confirm-deposit', 'pending-deposit', 'non-deposit'] }
+        })
+      ]);
+      resultObject = {
+        issuedTickets: issuedTickets,
+        salesProceeds: salesProceeds * 3000,
+        confirmedDeposit: confirmedDeposit,
+        pendingDeposit: pendingDeposit,
+        nonDeposit: nonDeposit,
+        entered: entered,
+        notEntered: notEntered
+      };
+      return res.custom200SuccessData(resultObject);
+      // return res.json({ success: true, data: { user: '찬진' } });
+    } catch (err) {
+      if (err instanceof CustomError) {
+        return next(err);
+      }
+      return next(new ServerCommonError(err));
+    }
+  }
+);
+
+module.exports = { RouteAdminTicketsInfo };
